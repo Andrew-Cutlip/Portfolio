7bb0ff232442286b4990bce6577e7342bda73108
Andrew Cutlip <snakeeye10@gmail.com>
2020-08-05 06:22:57 -0400

    Serves index.html

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.use(express.static('../public'))
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 9000
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
 
